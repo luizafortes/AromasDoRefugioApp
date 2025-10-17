b703e29951766ff47fd9cb45c4af26784c9800bc
Luiza Fortes <luizafortes@gmail.com>
2025-10-17 07:23:55 -0300

    Configuração das bibliotecas de documentação da API (Swagger e Scalar)

--- AromasDoRefugioApp.API/Program.cs
+++ AromasDoRefugioApp.API/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+//Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,6 +19,13 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+//Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+
+//Scalar
+app.MapScalarApiReference(s => s.WithTheme(ScalarTheme.BluePlanet));
 
 app.UseAuthorization();
 
